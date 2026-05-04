7a3692e66364fba7cf3f72e657958d234bc36317
aiya <aichkaxxx2007@gmail.com>
2026-05-04 12:35:55 +0500

    fixx variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,47 @@
 variable "project_name" {
+  type    = string
   default = "microservices-sre"
+}
+
+# Database
+variable "database" {
+  type = object({
+    host     = string
+    port     = number
+    name     = string
+    user     = string
+    password = string
+    mode     = string 
+  })
+}
+
+# Service ports
+variable "services" {
+  type = object({
+    auth_port  = number
+    order_port = number
+  })
+}
+
+variable "nginx_port" {
+  type    = number
+  default = 80
+}
+
+# Observability
+variable "observability" {
+  type = object({
+    prometheus_port = number
+    grafana_port    = number
+  })
+}
+
+# Network configuration
+variable "network" {
+  type = object({
+    name = string
+  })
+  default = {
+    name = "microservices-network"
+  }
 }
